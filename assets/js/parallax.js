fd3dd70b8ad6b264081ce002d3a51738c9576073
Alexandra Travkina <hello@fixel.ru>
2020-01-08 20:04:27 +0700

    Updated from Publii

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -8,7 +8,6 @@ function parallax(){
     var scrolled = $(window).scrollTop();
     $('header.cover .image').css('top',-(scrolled*0.05)+'px');
     $('header.cover .image .overlay').css('opacity',0.75+(scrolled*0.005)/10);
-    $('header.cover .welcome').css('top',0+(scrolled*0.05)+'%');
-    $('header.cover .welcome').css('opacity',1-(scrolled*0.01)/10);
-    $('header.cover .welcome').css('opacity',1-(scrolled*0.01)/10);
+    $('header.cover .head').css('top',0+(scrolled*0.05)+'%');
+    $('header.cover .head').css('opacity',1-(scrolled*0.01)/10);
 };
