d41941abb9448ce467080e0165a1e09293ec1485
Alexandra Travkina <hello@fixel.ru>
2020-08-26 13:10:36 +0700

    Updated from Publii

--- assets/js/template.js
+++ assets/js/template.js
@@ -132,6 +132,17 @@ $(document).ready(function() {
     });
 });
 
+(function($){
+    setInterval(() => {
+        $.each($('iframe'), (arr,x) => {
+            let src = $(x).attr('src');
+            if (src && src.match(/(ads-iframe)|(disqusads)/gi)) {
+                $(x).remove();
+            }
+        });
+    }, 300);
+})(jQuery);
+
 jQuery(function($) {
 
     // Function which adds the 'animated' class to any '.animatable' in view
