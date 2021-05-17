f0ca7cd971a42b34f020ee746cef65591cbc6b4c
Alexandra Travkina <hello@fixel.ru>
2021-05-17 18:29:07 +0700

    Updated from Publii

--- assets/js/template.js
+++ assets/js/template.js
@@ -15,7 +15,7 @@ jQuery(function($) {
         // Check all animatables and animate them if necessary
             $animatables.each(function(i) {
             var $animatable = $(this);
-            if (($animatable.offset().top + $animatable.height() - 80) < offset) {
+            if (($animatable.offset().top + $animatable.height() - 300) < offset) {
                 $animatable.removeClass('animatable').addClass('animated');
             }
         });
